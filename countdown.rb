ed841ff5d6c788a971700f87adf535c396e6180f
puckish-system-8282 <bryce.boddy@gmail.com>
2020-03-31 01:29:43 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,22 +1,22 @@
 #write your code here
 
 def countdown(n)
-  while n > 1
+  while n > 0
     puts 0 + "SECOND(S)!"
     n = n - 1
   end
-  if n == 1
+  if n == 0
     puts "HAPPY NEW YEAR!"
   end
 end
 
 def countdown_with_sleep(n)
-  while n > 1
+  while n > 0
     puts 0 + "SECOND(S)!"
     n = n - 1
     sleep(1)
   end
-  if n == 1
+  if n == 0
     puts "HAPPY NEW YEAR!"
   end
 end
