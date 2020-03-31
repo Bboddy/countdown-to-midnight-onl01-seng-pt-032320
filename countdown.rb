d85bb6afe9e18894b921b1a066eea2e5a57247f2
puckish-system-8282 <bryce.boddy@gmail.com>
2020-03-31 01:22:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown(s)
+  while s > 0
+    puts 0 + "SECOND(S)!"
+    s = s - 1
+end
+
+def countdown_with_sleep(s)
+  while s > 0
+    puts 0 + "SECOND(S)!"
+    s = s - 1
+    sleep(1)
 end
